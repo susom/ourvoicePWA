42ade16a09b717208bc9a4d106b80b51c5b20d31
Irvin Szeto <irvins@stanford.edu>
2022-11-22 12:12:27 -0800

    commiting first view (landing) along with other assets ported from cordova based app (might not need them all , delete later)

--- src/views/Landing/landing.js
+++ src/views/Landing/landing.js
@@ -1,31 +1,19 @@
-import React from "react";
-import { Button, Card } from "@blueprintjs/core";
-import "@blueprintjs/core/lib/css/blueprint.css";
-import { useNavigate } from "react-router-dom";
-
-const style = {
-    central:{
-        textAlign: 'center'
-    }
-}
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
 
 export function Landing(){
-    const navigate = useNavigate();
+    const [redirectNow, setRedirectNow] = useState(false);
 
+    setTimeout(() => setRedirectNow(true), 3000); //3 secs
 
-    const onClick = () => {
-        navigate(`/home`);
-
-    }
-    
-    return (
-        <div>
-            <Card >
-                <div style={style.central}>
-                    <h2>Ourvoice Landing</h2>
-                    <Button intent="success" text="Go home" onClick={onClick} />
-                </div>
-            </Card>
-        </div>
-    )
-}
+    return redirectNow ?
+        (
+            <Navigate to={{pathname: '/home'}} / >
+        )
+        :
+        (
+            <div id="splashScreen">
+                <h1>Our Voice Discovery Tool</h1>
+            </div>
+        );
+};
