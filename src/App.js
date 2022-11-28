8dcf0d8b3f7206c306f9dbc99f6e7ed7c1315b2b
Irvin Szeto <irvins@stanford.edu>
2022-11-28 07:57:35 -0800

    updateing app.js to include new views and contexts

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
 import './App.css';
 import {
-  BrowserRouter as Router,
+  BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
+
+import {DatabaseContextProvider} from "./contexts/Database";
+import {SessionContextProvider} from "./contexts/Session";
+import {ProjectContextProvider} from "./contexts/Project";
+
+import GlobalHeader from "./components/global_header";
+import GlobalFooter from "./components/global_footer";
+
 import Landing from './views/Landing';
 import Home from './views/Home';
+import Consent from './views/Consent';
+import Walk from './views/Walk';
+import Summary from './views/Summary';
+import Upload from './views/Upload';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Landing />} />
-        <Route path='/home' element={<Home />} />
-      </Routes>
-    </Router>
+    <DatabaseContextProvider>
+        <ProjectContextProvider>
+            <SessionContextProvider>
+                <BrowserRouter>
+                    <div className="view_box">
+                        <GlobalHeader/>
+                        <Routes>
+                            <Route path='/' element={<Landing />} />
+                            <Route path='/home' element={<Home />} />
+                            <Route path='/consent' element={<Consent />} />
+                            <Route path='/walk' element={<Walk />} />
+                            <Route path='/summary' element={<Summary />} />
+                            <Route path='/upload' element={<Upload />} />
+                        </Routes>
+                        <GlobalFooter/>
+                    </div>
+                </BrowserRouter>
+            </SessionContextProvider>
+        </ProjectContextProvider>
+    </DatabaseContextProvider>
   );
 }
 
